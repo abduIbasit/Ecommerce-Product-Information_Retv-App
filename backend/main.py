ca82eff9db820c4af20b4afdfa5f2e9c4071aa9f
unknown <Big woo>
2024-09-12 13:36:38 +0100

    fixed bug

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,29 @@
-# main.py
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from models import ProductRequest
 from scraper import product_scrape
 import uvicorn
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 @app.post("/")
 def base():
-    return {"status code":200, "message":"Hello!"}
+    return {"status code": 200, "message": "Hello!"}
 
 @app.post("/scrape-products/")
 async def scrape_products(request: ProductRequest):
     try:
+        print(request.product_name)
         # Call the product scraping function with request data
         products = product_scrape(
             product_name=request.product_name,
